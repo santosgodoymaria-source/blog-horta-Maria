bc9029c8edca348e2a9d487ef9c32e86c38a398c
santosgodoymaria-source <santos.godoy.maria@escola.pr.gov.br>
2026-08-18 19:21:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+
+    let curtiu = false;
+
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+
+            texto.textContent++;
+
+            curtiu = true;
+
+        } else {
+
+            texto.textContent--;
+
+            curtiu = false;
+
+        }
+
+    }
+
+});
